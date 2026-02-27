709cd0c3c37073bad72c7b754d3af33e4dffc712
Vatroslav Mastelić <vmaste00@fesb.hr>
2026-02-27 11:34:37 +0100

    add link to movie detail

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom"
+
 function MovieCard({ movie }) {
   return (
-    <div key={movie.id} className="movie-card">
+    <Link
+      className="movie-card"
+      to={`/movies/${movie.imdbID}`}
+      >
       <img src={movie.Poster} alt={movie.Title} style={{ width: "100%" }} />
       <p>{movie.Title}</p>
       <p>{movie.Year} {movie.Rating}</p>
-    </div>
+    </Link>
   )
 }
 
